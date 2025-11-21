b9594c48b75e5251ed8cfd9f53552bc6a8d48e93
Diego Bermeo <bdiego@est.ups.edu.ec>
2025-11-21 14:45:01 -0500

    prueba de commit

---
+++ static/js/clase.js
@@ -0,0 +1,9 @@
+// ...existing code...
+/*
+  ClassManager:
+  - Busca elementos con atributo data-clase
+  - Permite togglear la clase 'active'
+  - Provee API: init(), toggle(el), activate(id), onChange(cb)
+  Uso: <button data-clase="mi-clase" id="btn1">Toggle</button>
+*/
+
